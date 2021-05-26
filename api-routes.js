65e8e6e3a8d7d2eb83b5890183e12736fd4cf697
biancacamargomachado <bianca.camargo@acad.pucrs.br>
2021-05-26 20:13:34 -0300

    initial code

---
+++ api-routes.js
@@ -0,0 +1,9 @@
+let router = require('express').Router();
+router.get('/', function (req, res) {
+  res.json({
+    status: 'API Its Working',
+    message: 'Initial route',
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,147 +1,15 @@
-const express = require('express');
-const kcAdminClient = require('./keycloak/keycloak-config');
-const credentials = require('./keycloak-credentials');
+let express = require('express');
 
-const app = express();
+let app = express();
+let apiRoutes = require('./api-routes');
 
-const port = 8080;
+var port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+app.listen(port, function () {
+  console.log('Listening at http://localhost:' + port);
 });
 
-app.get('/test', function (req, res) {
-  res.send('Tested');
-});
 
-app.get('/auth', async (req, res) => {
-  const { username, password, grantType, clientId, totp, clientSecret } =
-    credentials;
-
-  try {
-    await kcAdminClient.auth({
-      username,
-      password,
-      grantType,
-      clientId,
-      totp,
-      clientSecret,
-    });
-
-    res.status(200).send({
-      accessToken: kcAdminClient.accessToken,
-      refreshToken: kcAdminClient.refreshToken,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-
-// Returns all users from keycloak
-app.get('/users', async function (req, res) {
-  let users;
-  try {
-    users = await kcAdminClient.users.find();
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-  
-  res.send(users);
-});
-
-// Returns an user by id
-app.get('/users/:id', async function (req, res, _) {
-  let user;
-  try {
-    user = await kcAdminClient.users.findOne({ id: req.params.id });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-
-  res.status(200).send(user);
-  
-});
-
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true,
-}));
-
-// Creates user
-app.post('/users', async function (req, res) {
-  const { username, email, firstName, lastName } = req.body;
-  let response;
-  try {
-    response = await kcAdminClient.users.create({
-      username,
-      email,
-      firstName,
-      lastName,
-      emailVerified: true,
-      enabled: true,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-
-  res.status(200).send(response);
-});
-
-// Updates the user data
-app.put('/users/:id', async function (req, res) {
-  const { firstName, lastName } = req.body;
-  let response;
-  try {
-    response = await kcAdminClient.users.update(
-      { id: req.params.id },
-      {
-        firstName,
-        lastName,
-        requiredActions: [],
-        emailVerified: true,
-      }
-    );
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-
-  res.status(200).send(response);
-});
-
-// Updates the user password
-app.patch('/users/:id', async function (req, res) {
-  const { password } = req.body;
-  const { id } = req.params;
-  let response;
-  try {
-    response = await kcAdminClient.users.update(
-      { id },
-      {
-        password,
-      }
-    );
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-
-  res.status(200).send(response);
-});
-
-// Deletes an user by id
-app.delete('/users/:id', async function (req, res) {
-  let response;
-  try {
-    response = await kcAdminClient.users.del({ id: req.params.id });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-
-  res.status(200).send(response);
-});
+app.use('/api', apiRoutes);
