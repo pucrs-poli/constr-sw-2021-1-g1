1e072d36ffc928ae4ff994aea02a2621e97209f3
biancacamargomachado <bianca.camargo@acad.pucrs.br>
2021-05-29 01:49:50 -0300

    initial building PATCH

--- api-routes.js
+++ api-routes.js
@@ -50,6 +50,55 @@ router.put('/buildings/:id', function (req, res) {
   );
 });
 
+/**
+ * Update some attributes of an existing building
+ */
+router.patch('/buildings/:id', async function (req, res) {
+  const db = require('./db/buildings');
+  const buildings = db.Mongoose.model(
+    'buildings',
+    db.BuildingsSchema,
+    'buildings'
+  );
+
+  const { floors, name, description, maxCapacity } = req.body;
+  let buildingReturn;
+  
+  try {
+   buildingReturn = await buildings.findById(req.params.id); 
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    res.end();
+    return;
+  }
+
+  if (floors) {
+    buildingReturn.floors = floors;
+  }
+
+  if (name) {
+    buildingReturn.name = name;
+  }
+
+  if (description) {
+    buildingReturn.description = description;
+  }
+
+  if (maxCapacity) {
+    buildingReturn.maxCapacity = maxCapacity;
+  }
+
+  try {
+    await buildingReturn.save();
+    res.status(200).json(buildingReturn);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    res.end();
+    return;
+  }
+  
+});
+
 // End of buildings routes
 
 // Start of rooms routes
--- db/buildings.js
+++ db/buildings.js
@@ -3,7 +3,7 @@ const mongoose = require('./connection');
 // _id is added by default
 const buildingsSchema = new mongoose.Schema(
   {
-    floors: { type: Number, min: 0, max: 2 },
+    floors: { type: Number, min: 0, max: 99 },
     name: String,
     description: String,
     maxCapacity: Number,
